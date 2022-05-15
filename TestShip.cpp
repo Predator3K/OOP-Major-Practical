18d93ad5483d06d0c1aa6bd5017886f09f8ea601
Caleb L <a1830321@adelaide.edu.au>
2022-05-15 07:29:45 +0000

    Make file add

--- TestShip.cpp
+++ TestShip.cpp
@@ -7,7 +7,9 @@ int main () {
     std::cout << "The ships name is " << Ship1.GetName() << " With length " << Ship1.GetLength() << std::endl;
     Ship Ship2("Destroyer", 1, 6);
     std::cout << "The ships name is " << Ship2.GetName() << " With length " << Ship2.GetLength() << std::endl;
+
     Ship* Ships = new Ship[3];
+
     Ship Ship3("Boat", 1, 3);
     Ship Ship4("Cruiser", 2, 4);
     Ship Ship5("Carrier", 3, 5);
@@ -18,6 +20,9 @@ int main () {
     for (int i = 0; i < 3; i++) {
         std::cout << "The ships name is " << Ships[i].GetName() << " With length " << Ships[i].GetLength() << std::endl;
     }
+
+    Ship* Ships2 = new Ship;
+    std::cout << "The ships name is " << Ships2->GetName() << " With length " << Ships2->GetLength() << std::endl;
 
     return 0;
 }
