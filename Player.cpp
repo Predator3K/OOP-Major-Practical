295a62ee22ddce1dff748424cae626f72a425d8c
Caleb L <a1830321@adelaide.edu.au>
2022-05-24 01:19:23 +0000

    Version 2

--- Player.cpp
+++ Player.cpp
@@ -89,7 +89,9 @@ void Player::Move(User* opponent){
 }
 
 void Player::DisplayBoard(User* opponent){
+    std::cout << "  0 1 2 3 4 5 6 7 8 9" << std::endl;
     for(int i=0;i<10;i++){
+        std::cout << i << " ";
         for(int j=0;j<10;j++){
             std::cout << ShipBoard[i][j] << " ";
         }
